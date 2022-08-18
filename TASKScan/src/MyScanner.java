44de2bd92bc6ebd1cfdafa36cbdbfe9fda98523a
Tetiana Huk <89546207+TetianaHuk@users.noreply.github.com>
2022-08-19 01:13:49 +0300

    Update MyScanner.java
    Changed the scanner to only one.

--- TASKScan/src/MyScanner.java
+++ TASKScan/src/MyScanner.java
@@ -3,14 +3,11 @@ public class MyScanner {
 
         public static void main(String[] args) {
 
-            Scanner scName = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
             System.out.println("What is your name?");
-            String name = scName.nextLine();
-
-            Scanner scAddress = new Scanner(System.in);
+            String name = sc.nextLine();
             System.out.println("Where do you live, " + name + "?");
-            String address = scAddress.nextLine();
-
+            String address = sc.nextLine();
             System.out.println("Total information about user:");
             System.out.println("Name of user - " + name);
             System.out.println("Address of user - " + address);
