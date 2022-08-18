c6624d5c1bd46031c9557100685aab71d90e1e45
Tetiana Huk <tanya.kedruk@gmail.com>
2022-08-18 21:01:43 +0300

    Task 5. UniversityGroup. Please, revise.

---
+++ UniversityGroups/src/UniversityGroups.java
@@ -0,0 +1,8 @@
+public class UniversityGroups {
+    public static void main(String[] args) {
+        Parameters firstGroup=new Parameters ("QA005", 2009);
+        Parameters secondGroup=new Parameters ("QA007", 2008, new String [] {"Jenna", "Casey", "Peter"});
+        firstGroup. getGroupDescription ();
+        secondGroup. getGroupDescription ();
+    }
+}
